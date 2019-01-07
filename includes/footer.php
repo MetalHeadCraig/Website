6776040a40010c1a317338de19aa87b8fca5c168
MetalHeadCraig <46405996+MetalHeadCraig@users.noreply.github.com>
2019-01-07 14:32:19 +0000

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div>
+        <center>
+    &copy; 2019 Website designed, created and maintained by Craig Anning & Tim Saunders &nbsp;
+    <i class="fab fa-youtube footer"></i>
+    <i class="fab fa-twitter footer"></i>
+    <i class="fab fa-facebook-square footer"></i>
+    <i class="fab fa-instagram footer"></i>
+        </center>
+    </div>
+</footer>
