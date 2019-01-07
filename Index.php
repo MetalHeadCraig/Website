fbb4b934a11d59c75c5bdef1463c97671b19e16f
MetalHeadCraig <46405996+MetalHeadCraig@users.noreply.github.com>
2019-01-07 14:22:40 +0000

    Update and rename Index.html to Index.php

---
+++ Index.php
@@ -0,0 +1,44 @@
+<?php include 'includes/overall/header.php'; ?>
+
+		<center><h1>Welcome to Show Name!</h1></center>
+
+		<div class="row">
+			<center><h3>Latest</h3></center>
+  			<div class="column">
+    			<div class="card">
+					<h4><b>Article 1</b></h4> 
+					<p>Some text information regarding Article 1 that was put up by one of us </p> 
+				</div>
+			</div>
+			<br>
+			<div class="column">
+    			<div class="card">
+					<h4><b>Article 2</b></h4> 
+					<p>Some text information regarding Article 2 that was put up by one of us </p> 
+				</div>
+			</div>
+			<br>
+			<div class="column">
+    			<div class="card">
+					<h4><b>Article 3</b></h4> 
+					<p>Some text information regarding Article 3 that was put up by one of us </p> 
+				</div>
+			</div>
+			<br>
+		</div>
+
+		<div class="row">
+			<center><h3>Social Feed</h3></center>
+  			<div class="social-column">
+    			<div class="card">
+					<h4><b>Facebook</b></h4>
+				</div>
+			</div>
+			<div class="social-column">
+    			<div class="card">
+					<h4><b>Twitter</b></h4>
+				</div>
+			</div>
+		</div>
+
+<?php include 'includes/overall/footer.php'; ?>
