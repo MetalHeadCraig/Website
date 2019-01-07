a5b43601aef7f577823d34747d941d4f38798d39
MetalHeadCraig <46405996+MetalHeadCraig@users.noreply.github.com>
2019-01-07 14:33:34 +0000

    Create header.php

---
+++ includes/overall/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<html lang="en">
+
+<?php include 'includes/head.php'; ?>
+
+	<body>
+
+		<!--header/nav-->
+
+		<?php include 'includes/header.php'; ?>
+
+		<!-- Sidebar -->
+
+		<aside>
+			<?php include 'includes/sidebar.php'; ?>
+		</aside>
+
+		<!-- Page content -->
+		
+		<div class="content">
