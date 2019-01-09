95b8e1c93742c47275c0b42fcfc76b1fa18da244
Unknown <saundersmtim@gmail.com>
2019-01-09 21:19:30 +0000

    JS Update

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,17 @@ $('#pages button').removeClass('active');
 $(buttonId).addClass('active');
 }
 
-function toggleSocial() {
-  $('#media').style.display = "block";
-  $('#social-call').style.display = "none";
-}
-
 var media = document.getElementById ('media');
 var social = document.getElementById ('social-call');
 
 window.onclick = function(event) {
-  if (event.target !== media) {
-  media.style.display = "none";
-  social.style.display = "block";
+  if (event.target == social) {
+    media.style.display = "block";
+    social.style.display = "none";    
+  }
+
+  else if (event.target !== media) {
+    media.style.display = "none";
+    social.style.display = "block";
   }
 }
