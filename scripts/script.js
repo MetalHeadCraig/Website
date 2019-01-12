bbf61b6502bb7983381aacd287f98fa552d47359
Tim <tim>
2019-01-12 20:09:18 +0000

    Updates
    Updates for styling and added podcasts button. Script not working for switching between smedia and pmedia

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,43 @@ $(buttonId).addClass('active');
 }
 
 
-var media = document.getElementById ('media');
+var smedia = document.getElementById ('smedia');
+var pmedia = document.getElementById ('pmedia');
 var social = document.getElementById ('social-call');
+var podcast = document.getElementById ('podcast-call');
 
-window.onclick = function(event) {
+/*window.onclick = function(event) {
   if (event.target == social) {
-    media.style.display = "block";
+    smedia.style.display = "block";
     social.style.display = "none";    
   }
 
-  else if (event.target !== media) {
-    media.style.display = "none";
+  else if (event.target !== smedia) {
+    smedia.style.display = "none";
     social.style.display = "block";
   }
+
+  else if (event.target == podcast) {
+    pmedia.style.display = "block";
+    podcast.style.display = "none";
+  }
+
+  else if (event.target !== pmedia) {
+    pmedia.style.display = "none";
+    podcast.style.display = "block";
+  }
+}
+*/
+
+switch(event) {
+  case (event.target == social): 
+    smedia.style.display = "block";
+    social.style.display = "none";
+  break;
+  case (event.target == podcast):
+    pmedia.style.display = "block";
+    podcast.style.display = "none";
+  break;
+  default: smedia.style.display = "none"
+  pmedia.style.display = "none"
 }
