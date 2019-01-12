81e3b4613c42487f7cf520b49421c748ba06b49b
Tim <tim>
2019-01-12 17:38:48 +0000

    Header PHP fix
    fixed the 'undefined php' error within the header inclusion

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,9 +4,12 @@
         <a href="https://twitter.com/snvgs"><i class="fab fa-twitter"></i></a>
         <a href="https://en-gb.facebook.com/pg/SNVGS"><i class="fab fa-facebook-square"></i></a>
 		<a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
-		<hr>
+    </div></div>
+
+<i class="fas fa-podcast accent shadow--6dp" id="podcast-call"></i>
+	<div id="media"><div class="podcast shadow--2dp">
 		<a href="#"><i class="fab fa-spotify"></i></a>
 		<a href="#"><i class="fab fa-soundcloud"></i></a>
 		<a href="#"><i class="fab fa-google"></i></a>
 		<a href="#"><i class="fas fa-podcast"></i></a>
-    </div></div>
+	</div></div>
