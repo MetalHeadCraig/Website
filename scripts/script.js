525699826bbe1aa1264749320697557e2c32e724
TSaunders87 <44271209+TSaunders87@users.noreply.github.com>
2019-01-16 21:25:50 +0000

    Script for external links
    Created a script for external links in new tabs. There is also one for opening external links whilst allowing exclusions in the case of requiring a store or something similar

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
+// Highlight the selected button in the navbar
 function selectButton(buttonId) {
 $('#pages button').removeClass('active');
 $(buttonId).addClass('active');
 }
 
-
+// Global variables for social media and podcast media buttons and content
 var smedia = document.getElementById ('smedia');
 var social = document.getElementById ('social-call');
 var pmedia = document.getElementById ('pmedia');
 var podcast = document.getElementById ('podcast-call');
 
-
+// display media content for social and podcast when buttons clicked, hide when anywhere else is clicked
 window.onclick = function(event) {
   if (event.target == social) {
     smedia.style.display = "block";
@@ -31,3 +32,46 @@ window.onclick = function(event) {
     podcast.style.display = "block";
   }
 }
+
+
+// Open all external links in new tabs
+$('a').each(function() {
+  var a = new RegExp('/' + window.location.host + '/');
+  if(!a.test(this.href)) {
+      $(this).click(function(event) {
+          event.preventDefault();
+          event.stopPropagation();
+          window.open(this.href, '_blank');
+      });
+  }
+});
+
+// This is where we can exclude instead of blanket if we need to (TESTED AND FUNCTIONAL)
+/* $(document).ready(function() {
+
+  $("a[href^=http]").each(function(){
+
+     // NEW - excluded domains list
+     var excludes = [
+       // this is where we enter domains that we don't want externally opened, a shop for example.
+     ];
+     for (i = 0; i < excludes.length; i++) {
+        if (this.href.indexOf(excludes[i]) != -1) {
+           return true; // continue each() with next link
+        }
+     }
+
+     if(this.href.indexOf(location.hostname) == -1) {
+
+          // this does nothing other than allow the click below to function
+          $(this).click(function() { return true; }); 
+
+          $(this).attr({
+              target: "_blank",
+              title: "Opens in a new window"
+          });
+
+          $(this).click(); // this makes it happen
+     }
+  })
+}); */
