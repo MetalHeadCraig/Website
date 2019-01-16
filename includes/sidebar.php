40f075346d45f1a766215cd93bd226450dc6642c
TSaunders87 <44271209+TSaunders87@users.noreply.github.com>
2019-01-16 20:52:25 +0000

    More CSS changes

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,4 +1,4 @@
-<i class="fas fa-users accent shadow--6dp" id="social-call"></i> 
+<i class="fas fa-users shadow--6dp" id="social-call"></i> 
     <div id="smedia"><div class="social shadow--2dp">
         <a href="https://www.youtube.com/channel/UCKm3WCRV3zQoZtlC7KR2dJw"><i class="fab fa-youtube"></i></a>
         <a href="https://twitter.com/snvgs"><i class="fab fa-twitter"></i></a>
@@ -6,7 +6,7 @@
 		<a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
     </div></div>
 
-	<i class="fas fa-podcast accent shadow--6dp" id="podcast-call"></i>
+	<i class="fas fa-podcast shadow--6dp" id="podcast-call"></i>
 	<div id="pmedia"><div class="podcast shadow--2dp">
 		<a href="#"><i class="fab fa-spotify"></i></a>
 		<a href="#"><i class="fab fa-soundcloud"></i></a>
