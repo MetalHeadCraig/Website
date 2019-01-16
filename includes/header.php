bce34673b892d0baa544a55b8532fa04759a4593
TSaunders87 <44271209+TSaunders87@users.noreply.github.com>
2019-01-16 20:22:05 +0000

    Style changes

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <header class="shadow--6dp">
 				<span id="name">Some Video Game Based Title</span>
-					<container id="pages">
-					<button id="home" onclick="selectButton('#home')" <?php if ($page == "home") {?> class="active" <?php ;} ?>>
+					<div id="nav">
+						<button id="home" onclick="selectButton('#home')" <?php if ($page == "home") {?> class="active" <?php ;} ?>>
 							<a href="index.php"><i class="fas fa-home"></i> Home</a></button>
 						<button id="articles" onclick="selectButton('#articles')" <?php if ($page == "article") {?> class="active" <?php ;} ?>>
 							<a href ="#"><i class="far fa-newspaper"></i> Articles</a></button>
@@ -9,6 +9,6 @@
 							<a href="about.php"><i class="fas fa-info-circle"></i> About</a></button>
 						<button id="contact" onclick="selectButton('#contact')" <?php if ($page == "contact") {?> class="active" <?php ;} ?>>
 						<a href="contact.php"><i class="fas fa-file-signature"></i> Contact</a></button>
-					</container>
+					</div>
 				<img src="images/placeholder-logo.png" id="logo">
 		</header>			
