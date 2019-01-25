085aead33714b030274a500110cfb1141a648500
ap17181 <Megadeth1987>
2019-01-25 11:25:45 +0000

    Article changes and Add aticle page

---
+++ admin/addarticle.php
@@ -0,0 +1,51 @@
+<?php
+$page = "Add article";
+include 'includes/header.php';
+
+//check if user is logged in
+if(!isset($_SESSION['user_id']) || !isset($_SESSION['logged_in'])){
+    //User not logged in. Redirect them back to the login.php page.
+    header('Location: login.php');
+    exit;
+}
+
+if(isset($_POST['addarticle'])){
+
+    //Retrieve the field values from our articles form.
+    $name       = ($_POST['name']);
+    $article    = ($_POST['article']);
+
+try {
+    $sql = "INSERT INTO article (aname, author, article) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);    
+    //Bind value.
+    $stmt->bindParam(1, $name);
+    $stmt->bindParam(2, $_SESSION['user_id']);
+    $stmt->bindParam(3, $article);  
+    //Execute.
+    $stmt->execute();  
+    echo "Article created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+}
+?>
+
+<h3>Add Article</h3>
+
+<form action="addarticle.php" method="post">
+
+<div class="input-container">
+    <input type="text" name="name" class="input-field" placeholder="Article Name" >
+</div>
+
+<div class="input-container">
+    <textarea name="article" class="input-field" rows="10" cols="30"> </textarea>
+</div>
+<button class="btn" type="submit" name="addarticle">Add article</button>
+
+</form>
+
+<?php include 'includes/footer.php'; ?>
--- articles.php
+++ articles.php
@@ -1,28 +1,48 @@
 <?php
 $page = "articles";
 include 'includes/overall/header.php'; 
+$limit = 5;
 ?>
 <link rel="stylesheet" href="css/articles.css" />
         <center><h1>Articles</h1></center>
-        
+
+       Which one is better for number of artices to display? (non-functioning at the moment) <br><br>
+    Display <a href="#">5</a> <a href="#">10</a> <a href="#">15</a> <a href="#">20</a> articles per page.<br>
+    Display 
+    <select name="articleNo">
+    <option value="5" selected>5</option>
+    <option value="10">10</option>
+    <option value="15">15</option>
+    <option value="20">20</option>
+    </select>
+    articles per page.
+
     <div class = "row">
 
 <?php
 
 try {
-    $data = $conn->query("SELECT * FROM article")->fetchAll();
+    $data = $conn->query("SELECT * FROM article ORDER BY id DESC LIMIT $limit")->fetchAll();
 
     foreach ($data as $row) {
-        echo "<div class=\"column shadow--3dp card\">";
-        echo "<h4>".$row['aname']."</h4>";
-        echo "<span style=\"float: left;\">Written by Admin</span>";
-        echo "<span class=\"date\">";
-        echo date('M d, Y',strtotime($row['adate']));
-        echo "</span><br />";
-        echo "<hr>";
-        echo $row['article']."<br><br>";
-        echo "<span style=\"float: right;\"><a href=\"#\">Read More</a></span>";
-        echo "</div>";
+        $sql = "SELECT firstname, lastname FROM users WHERE id = :id";
+        $stmt = $conn->prepare($sql);    
+            //Bind value.
+            $stmt->bindValue(':id', $row['author']);  
+            //Execute.
+            $stmt->execute();
+            $author = $stmt->fetch(PDO::FETCH_ASSOC);
+            ?>
+
+            <div class="column shadow--3dp card">
+            <h4> <?php echo $row['aname']; ?></h4>
+            <span style="float: left">Written by <a href="#"><?php echo $author['firstname'] . " " . $author['lastname']; ?></a></span>
+            <span class=date> <?php echo date('M d, Y',strtotime($row['adate'])); ?> </span><br>
+            <hr>
+            <?php echo $row['article'] . "<br><br>"; ?>
+            <span style="float: right"><a href="#">Read More</a></span>
+            </div>
+<?php
     }
 }
 catch(PDOException $e) {
