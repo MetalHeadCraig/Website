3012710c1198607b738e44c51181c4aff0ffe63d
ap17181 <Megadeth1987>
2019-02-09 09:18:00 +0000

    Link to my site

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer">
-    &copy; 2019 Website designed, created and maintained by Craig Anning & Tim Saunders &nbsp;
+    &copy; 2019 Website designed, created and maintained by <a href="www.craiganning.co.uk">Craig Anning</a> & Tim Saunders &nbsp;
     <a href="https://www.youtube.com/channel/UCKm3WCRV3zQoZtlC7KR2dJw"><i class="fab fa-youtube footer"></i></a>
     <a href="https://twitter.com/snvgs"><i class="fab fa-twitter footer"></i></a>
     <a href="https://en-gb.facebook.com/pg/SNVGS"><i class="fab fa-facebook-square footer"></i></a>
