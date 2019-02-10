749d3922d75cda4514a2f08481f7001babea85c0
Tim <saundersmtim@gmail.com>
2019-02-10 13:17:52 +0000

    Updates from live
    Complete updates of live versions. Header fixes, contact form fixes, iframe display bugs fixed etc..

--- index.php
+++ index.php
@@ -3,6 +3,14 @@ $page = "home";
 include 'includes/overall/header.php'; 
 ?>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 		<center><h1>Welcome to Gamenetics!</h1></center>
 
@@ -27,8 +35,17 @@ include 'includes/overall/header.php';
 							<span style="float: left">Written by <a href="#"><?php echo $author['firstname'] . " " . $author['lastname']; ?></a></span>
 							<span class=date> <?php echo date('M d, Y',strtotime($row['adate'])); ?> </span><br>
 							<hr>
-							<?php echo $row['article'] . "<br><br>"; ?>
-							<span style="float: right"><a href="#">Read More</a></span>
+                            
+  
+                            <?php
+                            $string = $row['article'];
+                            if (strlen($string) > 700) {
+                                $trimstring = substr($string, 0, 700). ' <a href="#">...Read More</a>';
+                            } else {
+                                $trimstring = $string;
+                            }
+                            echo $trimstring;                            
+                            ?>
 							</div>
 				<?php
 					}
@@ -40,7 +57,7 @@ include 'includes/overall/header.php';
 			?>
 		</div>
 		<div style="float: right;">
-			<a href="articles.php">View more articles from the Archive</a>
+			<!-- <a href="articles.php">View more articles from the Archive</a> -->
 		</div>
 			
 			<br>
@@ -49,14 +66,37 @@ include 'includes/overall/header.php';
 			<center><h3>Social Feed</h3></center>
   			<div class="scolumn shadow--3dp card">
 					<h4><b>Facebook</b></h4>
-					<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fsnvgs&tabs=timeline&width=400&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="400" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-			</div>
+<div class="fb-page" data-href="https://www.facebook.com/snvgs" data-tabs="timeline" data-width="300" data-height="500" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/snvgs" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/snvgs">The Video Games Show on Swindon 105.5</a></blockquote></div>
+            </div>            
 			<div class="scolumn shadow--3dp card">
 					<h4><b>Twitter</b></h4>
-					<a class="twitter-timeline" data-width="400" data-height="500" data-chrome="nofooter" href="https://twitter.com/snvgs?ref_src=twsrc%5Etfw">Tweets by Gamenetics</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+					<a class="twitter-timeline" data-width="300" data-height="500" data-chrome="nofooter" href="https://twitter.com/snvgs?ref_src=twsrc%5Etfw">Tweets by Gamenetics</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 			</div>
 		</div>
 		<br>
 
 <?php include 'includes/overall/footer.php'; ?>
+
+<script>
+$(window).on('resize', function() {
+   setTimeout(function(){CMSSpace.changeFBPagePlugin()}, 500);
+});
+ 
+$(window).on('load', function() {
+   setTimeout(function(){CMSSpace.changeFBPagePlugin()}, 1500);
+});
+
+CMSSpace.changeFBPagePlugin = function () {
+   //getting parent box width
+   var container_width = (Number($('.fb-column').width()) - Number($('.fb-column').css('padding-left').replace("px", ""))).toFixed(0);
+   //getting parent box height
+   var container_height = (Number($('.fb-column').height()) - (Number($('.fb-column-header').height()) + Number($('.fb-column-   header').css('margin-bottom').replace("px", "")) + Number(($('.fb-column').css('padding-top').replace("px", "")*2)))).toFixed(0);
+   if (!isNaN(container_width) && !isNaN(container_height)) {
+      $(".fb-page").attr("data-width", container_width).attr("data-height", container_height);
+   }
+   if (typeof FB !== 'undefined' ) {
+      FB.XFBML.parse();
+   }
+}
+</script>
 
